d8d3447885cedda5de35ad9662eb35366ab82a04
傅莘莘 <393763323@qq.com>
2016-10-15 23:42:21 +0800

    ntru-commint

---
+++ nativeNtl.cpp
@@ -0,0 +1,9 @@
+//
+//  nativeNtl.cpp
+//  NTL_test
+//
+//  Created by 傅莘莘 on 2016/10/15.
+//  Copyright © 2016年 傅莘莘. All rights reserved.
+//
+
+#include "nativeNtl.hpp"
---
+++ nativeNtl.hpp
@@ -0,0 +1,14 @@
+//
+//  nativeNtl.hpp
+//  NTL_test
+//
+//  Created by 傅莘莘 on 2016/10/15.
+//  Copyright © 2016年 傅莘莘. All rights reserved.
+//
+
+#ifndef nativeNtl_hpp
+#define nativeNtl_hpp
+
+#include <stdio.h>
+
+#endif /* nativeNtl_hpp */
